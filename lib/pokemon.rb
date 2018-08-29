bc06cb0feb0c2aa078f9a5e58e58c72725b1eabb
Ethel C <cofresi.ethel@gmail.com>
2018-08-29 18:11:04 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,12 +10,12 @@ class Pokemon
     Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
   end
 
-  def initialize(id:, name:, type:, db:, hp:)
-    @id = id,
-    @name = name,
-    @type = type,
-    @db = db,
-    @hp = hp
+  def initialize(id:, name:, type:, hp: nil, db:)
+    @id = id, 
+    @name = name, 
+    @type = type, 
+    @hp = hp, 
+    @db = db
   end
 
   def alter_hp(new_hp, db)
